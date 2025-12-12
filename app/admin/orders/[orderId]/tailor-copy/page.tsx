f45889ef12e909b04b1804e32b0e709df4c7d6c4
3MaheshKadam <maheshkadam9298@gmail.com>
2025-12-12 18:37:48 +0530

    sample

---
+++ app/admin/orders/[orderId]/tailor-copy/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>page</div>
+  );
+};
+
+export default page;//sample
+// Creating an optimized production build ...
+
+//  ✓ Compiled successfully in 7.1s
+
+//    Running TypeScript ...
+
+// Failed to compile.
+
+// Type error: File '/vercel/path0/app/admin/orders/[orderId]/admin-copy/page.tsx' is not a module.
+
+// Next.js build worker exited with code: 1 and signal: null
+
+// Error: Command "npm run b
